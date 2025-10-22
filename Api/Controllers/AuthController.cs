3eafe15d6b3719865aa262116d6e4336b1e4c529
siyavuyachagi <syavuya08@gmail.com>
2025-10-22 16:24:08 +0200

    feat: new model

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public class LoginModel
+        {
+            [Required]
+            public string Username { get; set; }
+            [Required]
+            public string Password { get; set; }
+            public bool RememberMe { get; set; } = false;
+        }
+
+        [HttpPost("login")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([FromBody] LoginModel loginModel)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        [HttpPost("refresh")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Refresh([FromBody] string refreshToken)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        [HttpPost("verify")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Verify([FromBody] string accessToken)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        [HttpPost("logout")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout([FromBody] string accessToken)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.NetworkInformation;
+
+namespace Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string LastName { get; set; } = new string("");
+        public string FirstName { get; set; } = new string("");
+        public string Gender { get; set; }
+        public string Bio { get; set; } = new string("");
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public string? Avatar { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+        public virtual string? Discriminator { get; set; }
+
+        //public Guid? OrganizationId { get; set; }
+        //[ForeignKey(nameof(OrganizationId))]
+        //public virtual Organization? Organization { get; set; }
+
+        public Guid? PhysicalAddressId { get; set; }
+        [ForeignKey(nameof(PhysicalAddressId))]
+        public virtual PhysicalAddress PhysicalAddress { get; set; }
+
+        //public virtual List<UserActivity> UserActivities { get; set; } = new();
+        //public virtual List<SocialLink> SocialLinks { get; set; } = new();
+
+        // Add this navigation property
+        public virtual ICollection<IdentityUserRole<string>> IdentityUserRoles { get; set; }
+        public virtual ICollection<IdentityRole> IdentityRoles { get; set; }
+        // If you need just the role names as a computed property
+        [NotMapped]
+        public List<string> UserRoles => IdentityRoles?.Select(r => r.Name).ToList() ?? new List<string>();
+    }
+}
