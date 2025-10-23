369a63d7195b06af97072227a43c7f191658c296
siyavuyachagi <syavuya08@gmail.com>
2025-10-23 11:55:34 +0200

    refactor: new entity models

---
+++ Domain/Entities/President.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class President: ApplicationUser
+    {
+        public Guid TenantId { get; set; }
+        [ForeignKey(nameof(TenantId))]
+        public virtual Tenant Tenant { get; set; }
+
+        public Guid OrganizationId { get; set; }
+        [ForeignKey(nameof(OrganizationId))]
+        public virtual Organization Organization { get; set; }
+    }
+}
--- Domain/Entities/Tenant.cs
+++ Domain/Entities/Tenant.cs
@@ -12,7 +12,10 @@ namespace Domain.Entities
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
-        public bool IsActive { get; set; }
+        // Owner association
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser ApplicationUser { get; set; }
 
         // Subscription info
         public Guid TenantSubscriptionId { get; set; }
@@ -23,10 +26,11 @@ namespace Domain.Entities
         public Guid BillingId { get; set; }
         [ForeignKey(nameof(BillingId))]
         public virtual Billing Billing { get; set; }
-
+         
         // Connection info
         public string SchemaName { get; set; }
         public string ConnectionString { get; set; }
+        public bool IsActive { get; set; }
 
         // System metadata
         public DateTime CreatedAt { get; set; }
